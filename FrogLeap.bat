e41bad74767ab108c0400093e477a8eb0a09a7a1
Poccer77 <doepfner.noah@gmail.com>
2023-05-31 21:39:53 +0200

    surely something changed

--- FrogLeap.bat
+++ FrogLeap.bat
@@ -1,8 +1,8 @@
 @echo off
-cd "src\"
+cd "out\production\FrogLeap"
 
 set /p INPUT="input: "
-java Main %INPUT%
+"C:\Program Files\Eclipse Adoptium\jdk-17.0.2.8-hotspot\bin\java.exe" Main %INPUT%
 
 timeout 5
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
